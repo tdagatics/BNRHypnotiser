b086d7a3ae88654141deca1755975895cc254745
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-16 10:49:29 -0700

    Initial Commit

---
+++ BNRHypnotiser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BNRHypnotiser
+//
+//  Created by Anthony Dagati on 10/16/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
